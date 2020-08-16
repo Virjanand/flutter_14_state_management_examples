2d53c446d25e2f3902d629715272e682dcd497bd
Virjanand <virjanand@virjanand.com>
2020-08-16 08:12:16 +0200

    Use provider to not only pass data, but also subscribe to changes from different points

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final String data = 'Top Secret Data';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text(data),
-          ),
-          body: Level1(data),
-        )
+    return ChangeNotifierProvider<Data>(
+      builder: (context) => Data(),
+      child: MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(
+              title: MyText(),
+            ),
+            body: Level1(),
+          )
+      ),
     );
   }
 }
 
-class Level1 extends StatelessWidget {
-  final String data;
+class MyText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(Provider.of<Data>(context,listen: false).data);
+  }
+}
 
-  Level1(this.data);
+class Level1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Level2(data),
+      child: Level2(),
     );
   }
 
 }
 
 class Level2 extends StatelessWidget {
-  final String data;
-
-  const Level2(this.data);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Level3(data),
+    return Column(
+      children: <Widget>[
+        MyTextField(),
+        Level3(),
+      ],
+    );
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (newText) {
+        Provider.of<Data>(context).changeString(newText);
+      },
     );
   }
 }
 
 class Level3 extends StatelessWidget {
-  final String data;
-
-  const Level3(this.data);
 
   @override
   Widget build(BuildContext context) {
-    return Text(data);
+    return Text(Provider.of<Data>(context).data);
   }
+}
+
+class Data extends ChangeNotifier {
+  String data = 'Some data';
+
+  void changeString(String newString) {
+    data = newString;
+    notifyListeners();
+  }
+
 }
